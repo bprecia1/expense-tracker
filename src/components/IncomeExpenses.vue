acad0ad8ce05814b0d6be8148e81267588af7a79
bprecia1 <107278642+bprecia1@users.noreply.github.com>
2024-02-03 18:04:13 -0800

    adding expenses

--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -6,7 +6,7 @@
             <p id="money-plus" class="money plus">+${{ income }}</p>
         </div>
         <div>
-            <h4>Expense</h4>
+            <h4>Expenses</h4>
             <p id="money-minus" class="money minus">-${{ expense }}</p>
         </div>
     </div>
